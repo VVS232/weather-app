79d2af6421c700dfb4e48ba5d5a781048164fd92
VVS <teenwolf23299@gmail.com>
2021-02-10 00:46:52 +0200

    basic header+css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import $ from "jquery";
+import "./style.css";
 
 const city = $("#city");
 async function getTemp() {
     let searchcity = city.val();
     let answer = await fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-            searchcity +
-            "&appid=" +
-            process.env.weatherAPI,
+        `http://api.openweathermap.org/data/2.5/weather?q=${searchcity}&appid=
+            ${process.env.weatherAPI}`,
         { mode: "cors" }
     );
     let citytemp = await answer.json();

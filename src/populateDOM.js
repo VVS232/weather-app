122de1b227cee5e6ee88938f487677891b9eddcf
VVS <teenwolf23299@gmail.com>
2021-02-10 21:50:37 +0200

    dates of the week work

--- src/populateDOM.js
+++ src/populateDOM.js
@@ -1,5 +1,8 @@
-import { getWholeInfo } from "./API-calls";
+/* eslint-disable import/prefer-default-export */
 import $ from "jquery";
+
+import { getWholeInfo } from "./API-calls";
+
 const city = $("#cityInput");
 const days = [
     "Воскресенье",
@@ -16,14 +19,59 @@ export async function populateInfo() {
 }
 
 function week(wholeInfo) {
-    setWeekDates();
+    setWeekDays();
 }
 
-function setWeekDates() {
+function setWeekDays() {
     const today = new Date();
+    setDayName(today);
+    setDayDate(today);
+    setDayMonth(today);
+}
+
+function setDayName(today) {
+    const days = [
+        "Воскресенье",
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+    ];
+    for (let i = 0; i < 7; i++) {
+        $(`#day${i}`)
+            .find(`.dayName`)
+            .text(days[(today.getDay() + i) % 7]);
+    }
+}
+
+function setDayDate(today) {
     for (let i = 0; i < 7; i++) {
         $(`#day${i}`)
             .find(".dayDate")
             .text(today.getDate() + i);
+    }
+}
+
+function setDayMonth(today) {
+    const months = [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ];
+    for (let i = 0; i < 7; i++) {
+        let day = new Date();
+        day.setDate(new Date().getDate() + i);
+        $(`#day${i}`).find(".dayMonth").text(months[day.getMonth()]);
     }
 }

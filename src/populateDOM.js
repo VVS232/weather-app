32840db9a255e86edb8667efb266a55e00e58f27
VVS <teenwolf23299@gmail.com>
2021-02-11 15:20:14 +0200

    pic display

--- src/populateDOM.js
+++ src/populateDOM.js
@@ -26,12 +26,21 @@ function week(wholeInfo) {
 export function setCurrent(current) {
     $("#currentCity").text(current.name);
     $("#currentTemp").text(current.main.temp);
+    $("#feelsLike").text("Чувствуется как " + current.main.feels_like);
+    $("#currentPic").attr({
+        src: `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`,
+    });
 }
 
 function hourlyWeather(wholeInfo) {
     const $hourly = $("#hourly");
     for (let i = 0; i < 24; i++) {
         let hour = $hourly.find(`#hour${i}`);
+        hour.find(".hourTime").after(
+            $("<img>").attr({
+                src: `http://openweathermap.org/img/wn/${wholeInfo.hourly[i].weather[0].icon}@2x.png`,
+            })
+        );
         hour.find(".hourTemp").text(wholeInfo.hourly[i].temp);
     }
 }

aa12dd862f337dd268d8b65b40c5cbecb2071954
VVS <teenwolf23299@gmail.com>
2021-02-11 16:35:38 +0200

    info for hours works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import "./style.css";
 import { populateInfo } from "./populateDOM";
+
 window.onload = populateInfo;
 $("#search").on("click", populateInfo);
 if (module.hot) {
--- src/populateDOM.js
+++ src/populateDOM.js
@@ -43,4 +43,27 @@ function hourlyWeather(wholeInfo) {
         );
         hour.find(".hourTemp").text(wholeInfo.hourly[i].temp);
     }
+    hourlyInfo(wholeInfo);
+}
+
+function hourlyInfo(wholeInfo) {
+    for (let i = 0; i < 24; i++) {
+        Array.from(document.getElementsByClassName("hour")).forEach((hour) => {
+            // get an hour number from ID by splitting the ID string;
+            let hourNumber = hour.id.split("r")[1];
+            hour.addEventListener("click", () => {
+                $("#infoTemp").text(wholeInfo.hourly[hourNumber].temp);
+                $("#infoFeels").text(wholeInfo.hourly[hourNumber].feels_like);
+                $("#infoPressure").text(wholeInfo.hourly[hourNumber].pressure);
+                $("#infoHumid").text(wholeInfo.hourly[hourNumber].humidity);
+                $("#infoWindSpeed").text(
+                    wholeInfo.hourly[hourNumber].wind_speed
+                );
+                $("#infoVisibility").text(
+                    wholeInfo.hourly[hourNumber].visibility
+                );
+                $("#infoPop").text(wholeInfo.hourly[hourNumber].pop);
+            });
+        });
+    }
 }
